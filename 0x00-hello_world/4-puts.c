52934a66255b316ec538b8efa1bcdc51f89106ca
danielchinome <danielchinome987@gmial.com>
2019-09-18 23:36:18 +0000

    C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main (void)
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-puts("\"Pogramming is like building a multilingual puzzle");
-return(0);
+	puts("\"Pogramming is like building a multilingual puzzle");
+	return  (0);
 }
