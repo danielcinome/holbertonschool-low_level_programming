125b0234d4e73a4932c0a33854b345953579ce1d
danielchinome <danielchinome987@gmial.com>
2019-09-20 00:28:13 +0000

    prints the numbers from 00 to 99.

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,19 +10,17 @@ int main(void)
 	int num2;
 
 	for (num = 48 ; num <= 57 ; num++)
-	{
 	for (num2 = 48 ; num2 <= 57 ; num2++)
 	{
 	putchar(num);
 	putchar(num2);
-	if (num2 != 57)
+	if (num != 57 || num2 != 57)
 	{
 	putchar(',');
 	putchar(' ');
 	}
 	else
 	{
-	}
 	}
 	}
 	putchar('\n');
