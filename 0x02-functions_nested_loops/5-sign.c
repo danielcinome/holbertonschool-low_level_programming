6ae821755148599df4cf6fbc23b535b8ce35374a
danielchinome <danielchinome987@gmial.com>
2019-09-24 15:05:56 +0000

    function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,34 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_sign - check the code for Holberton School students.
+ * @n: return the sign of a number.
+ * Return: +, 1 || 0, 0 || +, 1 || - , -1 (/).
  */
 int print_sign(int n)
 {
+	int val = 0;
+
 	if (n > 48)
 	{
 	_putchar('+');
 	_putchar(',');
 	_putchar(' ');
-	return (1);
+	val = 1;
 	}
 	else if (n == 0)
 	{
 	_putchar('0');
-        _putchar(',');
-        _putchar(' ');
-        return (0);
+	_putchar(',');
+	_putchar(' ');
+	val = 0;
 	}
 	else
 	{
 	_putchar('-');
-        _putchar(',');
-        _putchar(' ');
-        return (1);
+	_putchar(',');
+	_putchar(' ');
+	val = 1;
 	}
+	return (val);
 }
