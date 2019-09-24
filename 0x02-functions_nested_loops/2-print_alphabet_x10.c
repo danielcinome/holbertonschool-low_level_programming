00533a821db3604188872d0be29d5f626db6ebb1
danielchinome <danielchinome987@gmial.com>
2019-09-24 13:48:06 +0000

    function that prints 10 times the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet_x10 - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-	int v=0;
+	int v = 0;
 
 	while (v < 10)
 	{
@@ -19,5 +19,5 @@ void print_alphabet_x10(void)
 		}
 	v++;
 	_putchar('\n');
-	} 
+	}
 }
