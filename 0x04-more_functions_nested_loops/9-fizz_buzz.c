ce13429d2228d97bba89628a352ae51c47735bf1
danielchinome <danielchinome987@gmial.com>
2019-09-26 22:22:44 +0000

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
-* main - Fizz-Buzz test.
+* main- Fizz-Buzz test.
+* Return: 0.
 */
 
 int main(void)
@@ -10,16 +11,17 @@ int main(void)
 
 	for (val = 1 ; val <= 100 ; val++)
 	{
-	if (!(val % 3 == 0) && !(val % 5 == 0))
-	printf("%d", val);
-	if ((val % 3 == 0) && !(val % 5 == 0))
-	printf("Fizz");
-	if ((val % 5 == 0) && !(val % 3 == 0))
-	printf("Buzz");
-	if ((val % 5 == 0) && (val % 3 == 0))
-	printf("FizzBuzz");
+		if (!(val % 3 == 0) && !(val % 5 == 0))
+			printf("%d", val);
+		if ((val % 3 == 0) && !(val % 5 == 0))
+			printf("Fizz");
+		if ((val % 5 == 0) && !(val % 3 == 0))
+			printf("Buzz");
+		if ((val % 5 == 0) && (val % 3 == 0))
+			printf("FizzBuzz");
+		if (val != 100)
+			printf(" ");
 	}
-	printf(" ");
-	printf("\n");
+		printf("\n");
 	return (0);
 }
