cb2a89c74743e9df6210f82cdad8d812833f307a
danielcinome <danielcinome@github.com>
2019-09-30 23:20:19 -0500

    need to finish

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _strlen - returns the length of a string.
+* @*s : pointers
+* Return : value.
+*/
+
+int _strlen(char *s)
+{
+	char *str;
+
+	str = *s;
+	return (*str);
+}
