5de98fd89d34cf90b5835a25b3deeaf0a0db6e6d
danielcinome <danielcinome@github.com>
2019-10-02 23:57:29 -0500

    ed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* *_strcpy - string pointed to by src.
+* @dest : pointer
+* @src : pointyer
+* Return: 0
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (dest);
+}
