d604d80a9c39d1611cd4574f767c6ed50b4357a3
danielchinome <danielchinome987@gmial.com>
2019-10-03 18:16:11 +0000

    function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* *_strcat - function that concatenates two strings
+* @dest : pointer
+* @src : pointer
+* Return: val dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *pos;
+
+	pos = dest;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *dest + *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	dest = pos;
+	return (dest);
+}
