ed35206c9196402ed47f9fc58d05a28c8f9d28f8
danielchinome <danielchinome987@gmial.com>
2019-10-04 00:54:04 +0000

    changes all lowercase letters of a string to uppercase.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* *string_toupper - changes
+* @*: pointer
+* Return: val
+*/
+
+char *string_toupper(char *n)
+{
+	int c = 0;
+
+
+	while (n[c] != '\0')
+	{
+		if (n[c] >= 97 && n[c] <= 122)
+			n[c] = n[c] - 32;
+	c++;
+	}
+	return (n);
+}
