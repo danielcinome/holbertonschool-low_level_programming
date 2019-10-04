3de2679a580c1e48a8585601457a28e11f42d390
danielchinome <danielchinome987@gmial.com>
2019-10-04 19:17:18 +0000

    function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+* *cap_string - function that capitalizes all words of a string.
+* @n: pointer
+* Return: val
+*/
+
+char *cap_string(char *n)
+{
+	int c = 0;
+
+	while (n[c] != 0)
+	{
+		if (n[c] == '\t')
+			n[c] = 32;
+
+		if ((n[c] >= 32 && n[c] <= 47) || (n[c] >= 58 && n[c] <= 63)
+		|| (n[c] >= 123 && n[c] <= 126) || (n[c] == '\n'))
+		{
+			c++;
+			if (n[c] >= 97 && n[c] <= 122)
+			{
+				n[c] = n[c] - 32;
+			}
+			else
+			{
+			}
+		}
+		else
+		{
+			c++;
+		}
+	}
+	return (n);
+}
