24fde05be91964fbbb7ab32437ad232c7a6cdb66
danielcinome <danielcinome@github.com>
2019-10-05 11:40:08 -0500

    function that encodes a string into 1337.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+* *leet - function replace letter by number
+* @n : pointer
+* Return : val 
+*/
+
+char *leet(char *n)
+{
+	char letter[] = {"aAeEoOtTlL"};
+	char number[] = {"4433007711"};
+	int i = 0;
+	int il = 0;
+	int in = 0;
+
+	while (n[i] != '\0')
+	{
+		while (letter[il] != '\0')
+		{
+			if (n[i] == letter[il])
+			{
+				n[i] = number[il];
+			}
+			else
+			{
+				il++;
+				in++;
+			}
+		}
+	i++;
+	il = 0;
+	in = 0;
+	}
+	return (n);
+}
