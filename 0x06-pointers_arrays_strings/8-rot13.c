9600f61cd51a1b9f0a5222f946a9f00f5e452395
danielcinome <danielcinome@github.com>
2019-10-05 13:33:13 -0500

    function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* rot13 - funtion using rot13
+* @n : pointer
+* Return: val
+*/
+
+char *rot13(char *n)
+{
+	char al[] = {"aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"};
+	char rt[] = {"nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM"};
+	int i = 0;
+	int a = 0;
+
+	while (n[i] != '\0')
+	{
+	a = 0;
+		while (al[a] != '\0')
+		{
+			if (n[i] == al[a])
+				n[i] = rt[a];
+
+			a++;
+		}
+	i++;
+	}
+	return (n);
+}
