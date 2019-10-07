58f9d75707520add8e174608b0f6c8d8c7d9065f
danielchinome <danielchinome987@gmial.com>
2019-10-07 20:38:06 +0000

    searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* *_strpbrk - searches a string for any of a set of bytes.
+* @s : pointer
+* @accept : pointer
+* Return: s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s != '\0')
+	{
+		while (accept[a] != '\0')
+		{
+			if (*s == accept[a])
+			break;
+			a++;
+		}
+	if (*s == accept[a])
+	break;
+	a = 0;
+	s++;
+	}
+	return (s);
+}
