8da92663619fc62bd100a2d82cd816b855fc8cb3
danielcinome <danielcinome@github.com>
2019-10-07 22:33:58 -0500

    prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_chessboard -prints the chessboard. 
+* @a: pointer
+* @8: column
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int j, i;
+
+	for (j = 0 ; j < 8 ; j++)
+	{
+		for (i = 0 ; i < 8; i++)
+		{
+			_putchar (a[j][i]);
+		}
+	_putchar ('\n');
+	}
+}
