28a8f8aa4cf6ea6637672e55cc8edc8db6f5c914
danielcinome <danielcinome@github.com>
2019-10-07 23:54:36 -0500

    falta

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_diagsums - function
+* @a : pointer
+* @size : size
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	char r;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0; j < size ; j++)
+		{
+			r = a[i][j];
+		}
+	}
+}
