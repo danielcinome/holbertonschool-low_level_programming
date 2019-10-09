b1fbb2859f8ba77c3963550e22c47c01021e997b
danielchinome <danielchinome987@gmial.com>
2019-10-09 16:51:03 +0000

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - prints a string
+* @s : pointer
+*/
+
+void _puts_recursion(char *s)
+{
+	_putchar (*s);
+	s++;
+	if (*s != 0)
+	{
+	_puts_recursion(s);
+	}
+	else
+	{
+	_putchar ('\n');
+	}
+}
