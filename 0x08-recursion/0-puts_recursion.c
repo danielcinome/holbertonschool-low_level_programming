fe064d923205fed5f47c3a23f5686948e51ec763
danielchinome <danielchinome987@gmial.com>
2019-10-09 22:53:18 +0000

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,17 +7,16 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar (*s);
-	s++;
-
 	if (*s != '\0')
 	{
+		_putchar (*s);
+		s++;
 		_puts_recursion(s);
+		return;
 	}
 
 	else
 	{
 	_putchar ('\n');
 	}
-	return;
 }
