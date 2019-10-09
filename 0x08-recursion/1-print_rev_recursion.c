5b7bd61ebb5cc86c4608317015663b37aaf7cd19
danielchinome <danielchinome987@gmial.com>
2019-10-09 19:01:58 +0000

    prints a string in reverse.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _print_rev_recursion - prints a string in reverse.
+* @s : pointer
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		++s;
+		_print_rev_recursion(s);
+		--s;
+		_putchar (*s);
+	}
+	else
+	{
+		return;
+	}
+}
