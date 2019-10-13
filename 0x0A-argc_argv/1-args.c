b82c99221fbd7107f5517dfe10afc316418b2abf
danielcinome <danielchinome987@gmial.com>
2019-10-12 22:01:23 -0500

    check 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the number of arguments passed into it.
+* @argc : t argumento
+* @argv : vector de argumento
+* Return: val 0
+*/
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
