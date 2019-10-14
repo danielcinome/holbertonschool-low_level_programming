dfc9f63042186c57d57180f1a059813e0d6bdfe8
danielcinome <danielchinome987@gmial.com>
2019-10-13 22:28:20 -0500

    Task complete

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+void operation(int val);
+
 /**
 * main - change for an amount of money.
 * @argc : t argumento
@@ -8,8 +10,7 @@
 */
 int main(int argc, char *argv[])
 {
-	int c;
-	int mul = 0,  val = 0, t = 0;
+	int c, val = 0;
 
 	if (argc >= 2)
 	{
@@ -18,37 +19,7 @@ int main(int argc, char *argv[])
 			if ((atoi(argv[c])) > 0)
 			{
 				val = (atoi(argv[c]));
-				if (val / 25 || val % 25 == 0)
-				{
-					mul = (val / 25);
-					t = t + mul;
-					val = val - (25 * mul);
-				}
-				if (val / 10 || val % 10 == 0)
-				{
-					mul = (val / 10);
-					t = t + mul;
-					val = val - (10 * mul);
-				}
-				if (val / 5 || val % 5 == 0)
-				{
-					mul = (val / 5);
-					t = t + mul;
-					val = val - (5 * mul);
-				}
-				if (val / 2 || val % 2 == 0)
-				{
-					mul = (val / 2);
-					t = t + mul;
-					val = val - (2 * mul);
-				}
-				if (val / 1 || val % 1 == 0)
-				{
-					mul = (val / 1);
-					t = t + mul;
-					val = val - mul;
-				}
-			printf("%d\n", t);
+				operation(val);
 			}
 			else
 			{
@@ -63,4 +34,47 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	return (0);
+}
+
+/**
+* operation - operation.
+* @val : parametro
+*/
+void operation(int val)
+{
+	int t = 0, mul = 0;
+
+	if (val / 25 || val % 25 == 0)
+	{
+		mul = (val / 25);
+		t = t + mul;
+		val = val - (25 * mul);
+	}
+	if (val / 10 || val % 10 == 0)
+	{
+		mul = (val / 10);
+		t = t + mul;
+		val = val - (10 * mul);
+	}
+	if (val / 5 || val % 5 == 0)
+	{
+		mul = (val / 5);
+		t = t + mul;
+		val = val - (5 * mul);
+	}
+	if (val / 2 || val % 2 == 0)
+	{
+		mul = (val / 2);
+		t = t + mul;
+		val = val - (2 * mul);
+	}
+	if (val / 1 || val % 1 == 0)
+	{
+		mul = (val / 1);
+		t = t + mul;
+		val = val - mul;
+	}
+
+	printf("%d\n", t);
+
 }
