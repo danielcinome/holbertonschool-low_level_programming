ed2f6d18c05beeab59e63c7cbe11937224625367
Holberton <holberton@Holbertons-iMac-2.local>
2019-10-15 15:45:23 -0500

    returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* *_strdup - returns a pointer to a newly allocated space in memory
+* @str : Pointer in
+* Return: Copy of Pointer in
+*/
+
+char *_strdup(char *str)
+{
+	char *cop;
+	int c;
+
+	cop = malloc(*str);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (c = 0 ; str[c] != '\0' ; c++)
+	{
+		cop[c] = str[c];
+	}
+
+	return (cop);
+}
