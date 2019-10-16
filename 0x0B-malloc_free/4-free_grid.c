2546597d46d334446f642cd38f736db46ef9f69a
Holberton <holberton@Holbertons-iMac-2.local>
2019-10-16 15:22:02 -0500

    frees a 2 dimensional grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,30 +9,11 @@
 
 void free_grid(int **grid, int height)
 {
-	int **d;
-	int i, j;
-
-	d = malloc(sizeof(int *) * height);
+	int i;
 
 	for (i = 0 ; i < height ; i++)
 	{
-		d[i] = malloc(sizeof(int) * **grid);
-		if (d[i] == NULL)
-		{
-			for (i = i ; i >= 0 ; i--)
-			{
-				free(d[i]);
-			}
-			free(d);
-			}
-		}
-	for (i = 0 ; i < height ; i++)
-	{
-		for (j = 0 ; j < **grid ; j++)
-		{
-			free(d[j]);
-		}
-			free(d[i]);
-		}
-			free(d);
+		free(grid[i]);
+	}
+		free(grid);
 }
