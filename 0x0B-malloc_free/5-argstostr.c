11cc1544a9900b9da3ec847240eab87dc1bad3b4
danielcinome <danielchinome987@gmial.com>
2019-10-16 19:21:21 -0500

    correction checks

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (c = 0 ; c <= ac ; c++)
+	for (c = 0 ; c < ac ; c++)
 	{
 		for (c1 = 0 ; av[c][c1] != '\0' ; c1++)
 		{
