76a6bc032771e11bdd5c9a7bb8671546c2b1b705
Holberton <holberton@Holbertons-iMac-2.local>
2019-10-18 15:47:45 -0500

    reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,18 +5,39 @@
 
 /**
 * *_realloc - function that reallocates a memory block using malloc and free
-* @ptr :
-* @old_size : 
-* @new_size :
-* Return:
+* @ptr : Pointer to the memory previously allocated
+* @old_size : old size of memory
+* @new_size : new size of memory
+* Return: d or if fails NULL
 */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *d;
-	if (new_size == )
+	unsigned int i;
 
-	d = malloc(sizeof(char) * new_size)
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	d = malloc(new_size);
+
 	if (d == NULL)
-		returnn (NULL);	
+		return (NULL);
+
+	if (ptr == NULL)
+		return (d);
+
+	if (new_size > old_size)
+		for (i = 0 ; i < old_size ; i++)
+		{
+			d[i] = ((char *)ptr)[i];
+		}
+	free(ptr);
+	return (d);
 }
