6c68ec362cc099cf7bb8381c446c0dec9a870b83
Holberton <holberton@Holbertons-iMac-2.local>
2019-10-17 19:46:19 -0500

    falta

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *_realloc - function that reallocates a memory block using malloc and free
+* @ptr :
+* @old_size : 
+* @new_size :
+* Return:
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *d;
+	if (new_size == )
+
+	d = malloc(sizeof(char) * new_size)
+	if (d == NULL)
+		returnn (NULL);	
+}
