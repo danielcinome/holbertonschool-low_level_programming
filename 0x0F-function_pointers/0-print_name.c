e151800d14eae16aafaf0b9438f27373ef6a6605
Holberton <holberton@Holbertons-iMac-2.local>
2019-10-23 12:21:37 -0500

    function that prints a name.
    Prototype: void print_name(char

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+* print_name - function that prints a name.
+* @name : name print
+* @f : pointer called to function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
