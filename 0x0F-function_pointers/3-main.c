15001022e055e097f76c13d1a95401ee8218ddcc
danielcinome <danielchinome987@gmial.com>
2019-10-24 15:30:05 -0500

    correction deleted loop

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,30 +11,24 @@
 
 int main(int argc, char *argv[])
 {
-	char sig[] = {'+', '-', '*', '/', '%'};
-	int i = 0, j = 0, a = 0, b = 0, val = 0;
+	int a = 0, b = 0, val = 0;
 
 	if (argc < 4 || argc > 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	for (i = 0 ; argv[2][i] != '\0' ; i++)
+
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+	argv[2][0] != '/' &&  argv[2][0] != '%') || (argv[2][1] != '\0'))
 	{
-		for (j = 0 ; sig[j] != '\0' ; j++)
-		{
-			if (argv[2][i] == sig[j])
-				break;
-		}
-		if (j == 5 || i > 0)
-		{
-			printf("Error\n");
-			exit(99);
-		}
+		printf("Error\n");
+		exit(99);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if ((j == 3 && b == 0) || (j == 4 && b == 0))
+	if ((argv[2][0] == '/' && b == 0) || (argv[2][0] == '%' && b == 0))
 	{
 		printf("Error\n");
 		exit(100);
