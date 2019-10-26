4b3d8f6baf910f2d10fe35888fe0bc065c78af6d
Holberton <holberton@Holbertons-iMac-2.local>
2019-10-26 13:21:19 -0500

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -35,7 +35,6 @@ void print_all(const char * const format, ...)
 			printf("%s", str);
 			break;
 		default:
-			va_arg(p, int);
 			break;
 	}
 		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
