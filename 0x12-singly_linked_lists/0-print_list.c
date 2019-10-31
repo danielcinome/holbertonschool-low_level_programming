4d1ad781176a0ed143786250a1b81d4150c5102f
danielcinome <danielchinome987@gmial.com>
2019-10-31 17:58:32 -0500

    prueba

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* print_list - function that prints all the elements of a list_t list.
+* @h : pointer to lists_t
+* Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	if (h->str == NULL)
+		printf("(nil)");
+
+	while (h->next != NULL)
+	{
+		printf("[%i] %s", h->len, h->str);
+		h = h->next;
+	}
+	return (0);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef LISTS.H
-#define LISTS.H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,13 +16,12 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
 
 #endif
