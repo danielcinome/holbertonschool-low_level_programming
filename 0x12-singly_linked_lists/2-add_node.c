214bd58fe59a24539168b7645e174781901db73c
danielcinome <danielchinome987@gmial.com>
2019-10-31 21:34:12 -0500

    add prototype 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* add_node - function that adds a new node
+* @head: pointer double
+* @str: data add
+* Return: the address of the new element, or NULL if it failed
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i = 0;
+
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	while (str[i] != '\0')
+		i++;
+	new->len = i;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
