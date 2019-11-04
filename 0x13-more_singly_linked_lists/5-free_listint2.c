d32b5fb3ff8fcad8bab4cf3221991d30b46c9afe
danielcinome <danielchinome987@gmial.com>
2019-11-04 15:27:18 -0500

    task correct

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_listint2 - function that frees a listint_t list.
+* @head : pointer
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	while (*head)
+	{
+		aux = *head;
+		*head = aux->next;
+		free(aux);
+	}
+	free(*head);
+}
