407b0d17ece18c00ec1d510a6aff4e9c9cf194ed
danielcinome <danielchinome987@gmail.com>
2019-11-07 16:53:16 -0500

    task correct

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (n == '\0'|| index > 64)
+	if ((n == '\0') || (index > 64))
 		return (-1);
 	while (i != index)
 	{
