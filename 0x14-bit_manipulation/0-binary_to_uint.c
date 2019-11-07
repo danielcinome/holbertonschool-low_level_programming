c1bd48d29d343833f9fc089d1c5b0b2ee281ac12
danielcinome <danielchinome987@gmail.com>
2019-11-07 13:05:47 -0500

    add condition if null

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -46,6 +46,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int str = 0, i, j = 0;
 
+	if (b == '\0')
+		return (0);
+
 	str = _strlen(b);
 
 	for (i = str - 1 ; i >= 0 ; i--)
