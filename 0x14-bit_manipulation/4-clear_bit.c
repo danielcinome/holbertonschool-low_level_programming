33ee395d57aa9884bd2f58546712c0784b2c5b38
danielcinome <danielchinome987@gmial.com>
2019-11-07 19:59:50 -0500

    check task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* clear_bit -  sets the value of a bit to 0 at a given index.
+* @n : number integer
+* @index : starting from 0 of the bit you want to set
+* Return: 1 if it worked, or -1 if an error occurred
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (*n == '\0' || index > 64)
+		return (-1);
+	if (*n != 0)
+		*n = *n ^ (1 << index);
+	return (1);
+}
