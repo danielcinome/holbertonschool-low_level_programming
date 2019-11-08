d3154e00676b059efbb928aa7a4af23df87b7877
danielcinome <danielchinome987@gmial.com>
2019-11-07 21:30:36 -0500

    check task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+* con - funtion cont
+* @num : number
+* Return: counter
+*/
+
+int con(int num)
+{
+	int cont  = 0;
+
+	while (num)
+	{
+	cont += num & 1;
+	num >>= 1;
+	}
+	return (cont);
+}
+
+/**
+* flip_bits - bits you would need to flip to get from one nm to another.
+* @n : number
+* @m : number
+* Return: number of bits
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int val = 0;
+
+	val = con(n ^ m);
+	return (val);
+}
