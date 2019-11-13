4be0b0cd493b1dacc07e84fc7d74138aa3c06f29
danielcinome <danielchinome987@gmial.com>
2019-11-12 21:46:11 -0500

    check task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,45 +8,45 @@
 
 /**
 * main - copies the content of a file to another file.
-* @argc :
-* @argv :
-* Return:
+* @argc : counter of arguments
+* @argv : vector of arguments
+* Return: 0
 */
 
 int main(int argc, char *argv[])
 {
 	int wd, rd, val = 0, cwd, crd;
+	mode_t perm;
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "copies the content of a file to another file.");
-		exit (97);
-	}
-
+		dprintf(STDERR_FILENO, "copies the content of a file to another file.\n"),
+		exit(97);
 	rd = open(argv[1], O_RDONLY);
 	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE");
-		exit (98);
-	}
-	wd = open(argv[2], O_WRONLY | O_CREAT | O_APPEND, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n"),
+		exit(98);
+	perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+	S_IROTH | S_IWOTH;
+	wd = open(argv[2], O_WRONLY | O_CREAT, perm);
 	if (wd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE");
-		exit (99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n"),
+		exit(99);
+
 	while ((val = read(rd, buf, 1024)) > 0)
 	{
-		write(wd, buf, val)
+		if (write(wd, buf, val) != val)
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n"),
+			exit(99);
 	}
+	if (val == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n"),
+		exit(98);
+
 	cwd = close(wd);
 	crd = close(rd);
 	if (cwd == -1 || crd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
-		exit (100);
-	}
-	 
-	return (1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n"),
+		exit(100);
+	return (0);
 }
