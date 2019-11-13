a72f80a52d3d4a1d4f6f1eca9eafc7e3f0d8fb08
danielcinome <danielchinome987@gmial.com>
2019-11-12 22:07:23 -0500

    check task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,29 +24,29 @@ int main(int argc, char *argv[])
 		exit(97);
 	rd = open(argv[1], O_RDONLY);
 	if (rd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n"),
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
 	perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
 	S_IROTH | S_IWOTH;
-	wd = open(argv[2], O_WRONLY | O_CREAT, perm);
+	wd = open(argv[2], O_CREAT | O_WRONLY, perm);
 	if (wd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n"),
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]),
 		exit(99);
 
 	while ((val = read(rd, buf, 1024)) > 0)
 	{
 		if (write(wd, buf, val) != val)
-			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n"),
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]),
 			exit(99);
 	}
 	if (val == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n"),
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
 
 	cwd = close(wd);
 	crd = close(rd);
 	if (cwd == -1 || crd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n"),
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", rd),
 		exit(100);
 	return (0);
 }
