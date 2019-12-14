1ef0900c2175a09571a5b54c3cf67dc597012bb5
danielcinome <danielchinome987@gmail.com>
2019-12-14 13:07:24 -0500

    correction task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,11 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0, a = 0;
-
-	if (h == NULL)
-		return (0);
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-		a = h->n;
-		printf("%lu\n", a);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
