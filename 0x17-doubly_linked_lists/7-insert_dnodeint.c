77ce58bc6c1a7af9074e7535441737b3e0ceb920
danielcinome <danielchinome987@gmail.com>
2019-12-14 18:07:07 -0500

    correction task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - function that returns the number of elements in a linked
+ * @h : head
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
+
+/**
 * insert_dnodeint_at_index - inserts a new node at a given position.
 * @h : head
 * @idx : index of the list where the new node should be added
@@ -13,40 +35,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	dlistint_t *temp;
 	unsigned int i;
+	size_t num_node = 0;
 
-	new = *h;
 	if (h == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	temp = *h;
-	if (*h == NULL && idx == 0)
+	if (idx == 0)
 	{
-		new->next = NULL;
-		new->prev = NULL;
+		new = add_dnodeint(&temp, n);
 		*h = new;
+		return (*h);
 	}
-	for (i = 0 ; i < idx ; i++)
+	num_node = dlistint_len(temp);
+	if ((num_node - 1) == idx)
 	{
-		if (temp->next == NULL)
-			return (NULL);
-		temp = temp->next;
-	}
-	new->n = n;
-	new->next = temp;
-	if (idx > 0)
-	{
-		new->prev = temp->prev;
-		temp = temp->prev;
-		temp->next = new;
+		new = add_dnodeint_end(&temp, n);
+		return (*h);
 	}
 	else
 	{
-		new->prev = NULL;
-		temp->prev = new;
-		*h = new;
+		for (i = 0 ; i < idx ; i++)
+		{
+			temp = temp->next;
+		}
+		new->n = n;
+		temp->prev->next = new;
+		new->next = temp;
+		new->prev = temp->prev;
 	}
-	return (new);
+	return (*h);
 }
