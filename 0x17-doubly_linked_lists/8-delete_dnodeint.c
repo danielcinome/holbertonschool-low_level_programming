13dc9366043a2b2d56a390d031a59f4befed385c
danielcinome <danielchinome987@gmail.com>
2019-12-14 23:18:17 -0500

    correction task

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -42,6 +42,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 			ant = temp->prev;
 			ant->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = ant;
 			free(temp);
 		}
 	return (1);
