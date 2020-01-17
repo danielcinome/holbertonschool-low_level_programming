9cf624b2193b349a6391b3110d6aff95c098603f
danielcinome <danielchinome987@gmail.com>
2020-01-17 23:28:40 +0000

    add condition

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
 * hash_table_create - unction that creates a hash table.
 * @size : size of the array
@@ -11,16 +10,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
+	if (size <= 0)
+		return (NULL);
+
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-	{
 		return (NULL);
-	}
+
 	new_table->array = calloc(size, sizeof(char *));
 	if (new_table->array == NULL)
-	{
 		return (NULL);
-	}
-	
-	return new_table;
+
+	new_table->size = size;
+
+	return (new_table);
 }
