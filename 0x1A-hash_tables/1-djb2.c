c413ecdc31efb68a5a5ca3433a79e0439eecc41e
danielcinome <danielchinome987@gmail.com>
2020-01-17 23:32:56 +0000

    Correction betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 algorithm.
+ * @str: string "key"
+ * Return: value string
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
