5c44755130663c3900f96f12014d0357cee3951e
danielcinome <danielchinome987@gmail.com>
2020-01-17 22:28:19 +0000

    task completed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	new_table = calloc(size, sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+	new_table->array = calloc(size, sizeof(char *));
+	if (new_table->array == NULL)
+	{
+		return (NULL);
+	}
+	
 	return new_table;
 }
