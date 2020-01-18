76b2a608cfa45071078793d26d1243a23c28f46a
danielcinome <danielchinome987@gmail.com>
2020-01-18 22:23:21 +0000

    Completed task and add prototype in file.h

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht : hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	int i = 0;
+	int state = 0;
+
+	if (ht != NULL)
+		printf("{");
+		while (i <= 1024)
+		{
+			if (ht->array[i] != NULL)
+			{
+				temp = ht->array[i];
+				if (state == 1)
+					printf(", ");
+				while (temp)
+				{
+					printf("'%s': '%s'", temp->key, temp->value);
+					if (temp->next != NULL)
+						printf(", ");
+					temp = temp->next;
+				}
+				state = 1;
+			}
+			i++;
+		}
+	printf("}\n");
+}
